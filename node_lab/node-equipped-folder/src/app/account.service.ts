e2fe9f9918323120c86b0d710e5044f5c0217a49
That_Guy <eghove@gmail.com>
2019-07-13 10:27:49 -0500

    finishing services

--- node_lab/node-equipped-folder/src/app/account.service.ts
+++ node_lab/node-equipped-folder/src/app/account.service.ts
@@ -1,5 +1,8 @@
-import { stat } from "fs";
+import { LoggingService } from "./logging.service";
+import { Injectable } from "@angular/core";
 
+// injectable added to the service that will receive the injected service.
+@Injectable()
 export class AccountService {
   accounts = [
     {
@@ -16,12 +19,16 @@ export class AccountService {
     }
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
 
   }
 }
--- node_lab/node-equipped-folder/src/app/new-account/new-account.component.ts
+++ node_lab/node-equipped-folder/src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { AccountService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -17,6 +17,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
    this.accountService.addAccount(accountName, accountStatus);
-   this.loggingService.logStatusChange(accountStatus);
+  //  this.loggingService.logStatusChange(accountStatus);
   }
 }
