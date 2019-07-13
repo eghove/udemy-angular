24874154ac0f31d282c890ff34bbab35cbc042ae
That_Guy <eghove@gmail.com>
2019-07-13 13:08:45 -0500

    service assignment src

--- 5assignment_src_services-assignment-start/src/app/app.module.ts
+++ 5assignment_src_services-assignment-start/src/app/app.module.ts
@@ -4,20 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { CounterService } from './counter.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
-    NewAccountComponent
+    ActiveUsersComponent,
+    InactiveUsersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
