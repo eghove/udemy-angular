84a49cdd4531706f626d57511b2faf210173a782
That_Guy <eghove@gmail.com>
2019-08-09 06:38:49 -0500

    fixing bug in recipe service

--- 16ex_source/prj-observables-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ 16ex_source/prj-observables-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService} from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -28,7 +29,25 @@ export class RecipeEditComponent implements OnInit {
   }
   
   onSubmit() {
-    console.log(this.recipeForm);
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'], 
+    //   this.recipeForm.value['description'], 
+    //   this.recipeForm.value['imagePath'], 
+    //   this.recipeForm.value['ingredients']);
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount' : new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)] )
+      })
+    )
   }
 
   private initForm() {
@@ -47,17 +66,17 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              'name': new FormControl(ingredient.name),
-              'amount' : new FormControl(ingredient.amount)
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount' : new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
             })
           );
         }
       }
     }
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath' : new FormControl(recipeImagePath),
-      'description' : new FormControl(recipeDescription),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath' : new FormControl(recipeImagePath, Validators.required),
+      'description' : new FormControl(recipeDescription, Validators.required),
       'ingredients' : recipeIngredients
 
     });
--- 16ex_source/prj-observables-final/src/app/recipes/recipe.service.ts
+++ 16ex_source/prj-observables-final/src/app/recipes/recipe.service.ts
@@ -3,9 +3,12 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -37,5 +40,16 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+
   }
 }
