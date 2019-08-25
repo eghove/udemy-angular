25d6453cdebae5f132aba00becc82bab4707abeb
That_Guy <eghove@gmail.com>
2019-08-25 11:33:02 -0500

    starting authentication

--- 16ex_source/prj-observables-final/src/app/app.module.ts
+++ 16ex_source/prj-observables-final/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -30,13 +32,15 @@ import { RecipeService } from './recipes/recipe.service';
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
---
+++ 16ex_source/prj-observables-final/src/app/shared/data-storage.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map, tap } from 'rxjs/operators';
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipesService: RecipeService) { }
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http.put(
+      'https://ng-ang-recipebook.firebaseio.com/recipes.json',
+      recipes
+    ).
+      subscribe(response => {
+        console.log(response)
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+    .get<Recipe[]>('https://ng-ang-recipebook.firebaseio.com/recipes.json')
+    .pipe(map(recipes => {
+      return recipes.map(recipe => {
+        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+      })
+    }),
+    tap(recipes => {
+      this.recipesService.setRecipes(recipes)
+    })
+    )
+
+  }
+
+
+}
