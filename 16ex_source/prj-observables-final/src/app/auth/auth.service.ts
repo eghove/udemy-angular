964311ef225ed31db7d8d6a58f47bae7ddf27276
That_Guy <eghove@gmail.com>
2019-08-27 06:50:49 -0500

    setting up signup

---
+++ 16ex_source/prj-observables-final/src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+interface AuthResponseData {
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.API_KEY,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
+
+}
