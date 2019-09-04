fa39f4b168f68944d96265319fd084ea48dc2d21
That_Guy <eghove@gmail.com>
2019-09-04 07:03:53 -0500

    more error handling, user authentication

--- 16ex_source/prj-observables-final/src/app/auth/auth.service.ts
+++ 16ex_source/prj-observables-final/src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { catchError} from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError, tap} from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 interface AuthResponseData {
   idToken: string,
@@ -14,9 +15,12 @@ interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+
+  user = new Subject<User>()
+
   constructor(private http: HttpClient) {}
 
-  API_KEY: string ='null'
+  API_KEY: string =''
 
 
 
@@ -29,18 +33,18 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(errorRes => {
-        let errorMessage = "An unknown error occurred."
-        if (!errorRes.error || !errorRes.error.error ) {
-          return throwError(errorMessage);
+    ).pipe(
+      catchError(this.handleError), 
+      tap(resData => {
+          this.handleAuthentication(
+            resData.email, 
+            resData.localId, 
+            resData.idToken, 
+            +resData.expiresIn
+          )
         }
-        switch(errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = "This email already exists.";
-            break;
-        }
-        return throwError(errorMessage);
-    }))
+        )
+      )
   }
 
   login(email: string, password: string) {
@@ -50,7 +54,50 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     }
-    )
+    ).pipe(
+      catchError(this.handleError), 
+      tap(resData => {
+          this.handleAuthentication(
+            resData.email, 
+            resData.localId, 
+            resData.idToken, 
+            +resData.expiresIn
+          )
+        }
+        )
+      )
+  }
+
+  private handleAuthentication(email: string, userId:string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email, 
+      userId, 
+      token, 
+      expirationDate
+      );
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occurred."
+        if (!errorRes.error || !errorRes.error.error ) {
+          return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = "This email already exists.";
+            break;
+          case 'EMAIL_NOT_FOUND':
+            errorMessage = "This email not found";
+            break;
+          case 'INVALID_PASSWORD':
+            errorMessage = "Invalid password";
+            break;
+          
+        }
+        return throwError(errorMessage);
+
   }
 
 }
